12db33bc45589dfcc5383a7abc917fd8fb5b889d
jrice <jr16@iu.edu>
2021-09-25 16:07:11 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,24 @@
+function route(){
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#/","");
+    //pageID holds page name
+
+    if(!pageID){
+        MODEL.changePage("home");
+
+    }else{
+        MODEL.changePage(pageID);
+
+    }
+
+}
+
+function initlistener(){
+    $(window).on("hashchange",route);
+    route();
+
+}
+
+$(document).ready(function(){
+    initlistener();
+})
